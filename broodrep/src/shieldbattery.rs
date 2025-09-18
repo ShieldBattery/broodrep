b44c256eb9efabec8bf0b1812f79f8dc20592c95
Travis Collins <travis@tec27.com>
2025-09-17 22:41:34 -0700

    Add comments to ShieldBatteryData fields, restructure data storage slightly to avoid repetition.

--- broodrep/src/shieldbattery.rs
+++ broodrep/src/shieldbattery.rs
@@ -17,7 +17,7 @@ pub enum ShieldBatteryDataError {
 #[derive(Debug, Clone)]
 pub enum ShieldBatteryData {
     Version0(ShieldBatteryDataV0),
-    Version1(ShieldBatteryDataV1),
+    Version1(ShieldBatteryDataV0, ShieldBatteryDataV1),
 }
 
 #[derive(Debug, Clone)]
@@ -32,64 +32,64 @@ pub struct ShieldBatteryDataV0 {
 
 #[derive(Debug, Clone)]
 pub struct ShieldBatteryDataV1 {
-    // Same as v0 at the beginning
-    pub starcraft_exe_build: u32,
-    pub shieldbattery_version: String,
-    pub team_game_main_players: [u8; 4],
-    pub starting_races: [u8; 12],
-    pub game_id: u128,
-    pub user_ids: [u32; 8],
-    // New fields in v1
     game_logic_version: u16,
 }
 
 impl ShieldBatteryData {
+    /// The build number of the StarCraft executable used to play the game.
     pub fn starcraft_exe_build(&self) -> u32 {
         match self {
             ShieldBatteryData::Version0(data) => data.starcraft_exe_build,
-            ShieldBatteryData::Version1(data) => data.starcraft_exe_build,
+            ShieldBatteryData::Version1(data, _) => data.starcraft_exe_build,
         }
     }
 
+    /// The version string of the ShieldBattery client used to play the game.
     pub fn shieldbattery_version(&self) -> &str {
         match self {
             ShieldBatteryData::Version0(data) => &data.shieldbattery_version,
-            ShieldBatteryData::Version1(data) => &data.shieldbattery_version,
+            ShieldBatteryData::Version1(data, _) => &data.shieldbattery_version,
         }
     }
 
+    /// Which players were the "main" players in a team game (e.g. Team Melee).
     pub fn team_game_main_players(&self) -> &[u8; 4] {
         match self {
             ShieldBatteryData::Version0(data) => &data.team_game_main_players,
-            ShieldBatteryData::Version1(data) => &data.team_game_main_players,
+            ShieldBatteryData::Version1(data, _) => &data.team_game_main_players,
         }
     }
 
+    /// The starting race for each player in the game.
     pub fn starting_races(&self) -> &[u8; 12] {
         match self {
             ShieldBatteryData::Version0(data) => &data.starting_races,
-            ShieldBatteryData::Version1(data) => &data.starting_races,
+            ShieldBatteryData::Version1(data, _) => &data.starting_races,
         }
     }
 
+    /// The game's ID on ShieldBattery (a UUID as a u128).
     pub fn game_id(&self) -> u128 {
         match self {
             ShieldBatteryData::Version0(data) => data.game_id,
-            ShieldBatteryData::Version1(data) => data.game_id,
+            ShieldBatteryData::Version1(data, _) => data.game_id,
         }
     }
 
+    /// The ShieldBattery user IDs of the players ingame, in the same order as the players in the
+    /// replay header.
     pub fn user_ids(&self) -> &[u32; 8] {
         match self {
             ShieldBatteryData::Version0(data) => &data.user_ids,
-            ShieldBatteryData::Version1(data) => &data.user_ids,
+            ShieldBatteryData::Version1(data, _) => &data.user_ids,
         }
     }
 
+    /// The version of ShieldBattery game logic modifications used to play the game.
     pub fn game_logic_version(&self) -> Option<u16> {
         match self {
             ShieldBatteryData::Version0(_) => None,
-            ShieldBatteryData::Version1(data) => Some(data.game_logic_version),
+            ShieldBatteryData::Version1(_, data) => Some(data.game_logic_version),
         }
     }
 }
@@ -113,27 +113,21 @@ pub fn parse_shieldbattery_section(
     let mut user_ids = [0u32; 8];
     data.read_u32_into::<LE>(&mut user_ids)?;
 
+    let v0 = ShieldBatteryDataV0 {
+        starcraft_exe_build,
+        shieldbattery_version,
+        team_game_main_players,
+        starting_races,
+        game_id,
+        user_ids,
+    };
     if version == 0 {
-        Ok(ShieldBatteryData::Version0(ShieldBatteryDataV0 {
-            starcraft_exe_build,
-            shieldbattery_version,
-            team_game_main_players,
-            starting_races,
-            game_id,
-            user_ids,
-        }))
+        Ok(ShieldBatteryData::Version0(v0))
     } else
     /* if version >= 1 */
     {
         let game_logic_version = data.read_u16::<LE>()?;
-        Ok(ShieldBatteryData::Version1(ShieldBatteryDataV1 {
-            starcraft_exe_build,
-            shieldbattery_version,
-            team_game_main_players,
-            starting_races,
-            game_id,
-            user_ids,
-            game_logic_version,
-        }))
+        let v1 = ShieldBatteryDataV1 { game_logic_version };
+        Ok(ShieldBatteryData::Version1(v0, v1))
     }
 }
